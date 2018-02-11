1cd378f9cfd41ec1f9537ec439a8114fc3015d03
md-azsa <md.azsa@gmail.com>
2018-02-11 16:43:31 +0800

    Integrated the class components for Address Changed the help text for AddCommand help, affliated with new Address class components

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,11 +8,21 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String EXAMPLE = "123, Clementi Ave 3, #01-01, 422123";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must be [BLOCK], [STREET], [UNIT], [POSTAL_CODE]";
+    public static final String ADDRESS_VALIDATION_REGEX = ".+,.+,.+,.+";
+    public static final String COMMA = ",";
+
+    public static final int BLOCK_INDEX = 0;
+    public static final int STREET_INDEX = 1;
+    public static final int UNIT_INDEX = 2;
+    public static final int POSTAL_CODE_INDEX = 3;
 
     public final String value;
+    private Block block;
+    private Street street;
+    private Unit unit;
+    private PostalCode postalCode;
     private boolean isPrivate;
 
     /**
@@ -26,7 +36,30 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        parseAddressArgumentToComponents(trimmedAddress);
+        this.value = buildAddressByCombiningComponents();
+    }
+
+
+    /**
+     * Method removes the comma in the address argument
+     * Initializes the class components of Address: Block, Street, Unit, PostalCode
+     */
+    public void parseAddressArgumentToComponents (String trimmedAddress) {
+        String[] parsedAddress = trimmedAddress.split(",");
+        this.block = new Block(parsedAddress[BLOCK_INDEX].trim());
+        this.street = new Street(parsedAddress[STREET_INDEX].trim());
+        this.unit = new Unit(parsedAddress[UNIT_INDEX].trim());
+        this.postalCode = new PostalCode(parsedAddress[POSTAL_CODE_INDEX].trim());
+    }
+
+    /**
+     * Method glues every component together to form the address
+     *
+     * @return The address as String
+     */
+    public String buildAddressByCombiningComponents() {
+        return block + COMMA + street + COMMA + unit + COMMA + postalCode;
     }
 
     /**
